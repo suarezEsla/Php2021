d7088dfa4cf5261b5793bf1b67cd509149d230ba
Esla <suarezesla@gmail.com>
2021-09-21 12:30:09 +0200

    Delete 1.php

--- Ejercicios1/1.php
+++
@@ -1,6 +0,0 @@
-<php
-1.- Definir dos variables asignándoles un valor entero aleatorio entre 1 y 10 y mostrar su suma, su resta, su división, su multiplicación, módulo y potencia (ciclo for)
-
-
-
-?>
