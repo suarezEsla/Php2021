2f4cf1b35883ac16135a35c65a74b26990ed3ee2
Esla <suarezesla@gmail.com>
2021-10-12 13:23:03 +0200

    Update 1.php

--- Ejercicios3_arrays/1.php
+++ Ejercicios3_arrays/1.php
@@ -41,10 +41,32 @@ function minimo ($aleatorios){
     echo $numMin;
 }
 
+//Devuelve el número que mas veces se repite
+function valorRepetido ($aleatorios) {
+    $maxrepes = 0;
+    $valor =0;
+    for ($i = 0; $i < count($aleatorios); $i++) {
+        $veces = 0;
+        // Anoto cuantas veces se repite el elemento $i
+        for ($j = 0; $j < count($aleatorios); $j++) {
+            if ($aleatorios[$i] == $aleatorios[$j]) {
+                $veces++;
+            }    
+        }
+        if ($veces > $maxrepes) {
+            $valor = $aleatorios[$i];
+            $maxrepes = $veces;
+        }
+    }
+    return $valor;
+}
+
 echo '<h3>Máximo: </h3><br>';
 maximo($aleatorios);
 echo '<h3>Mínimo: </h3><br>';
 minimo($aleatorios);
+$repetido = valorRepetido($aleatorios);
+    echo "<br><h3>Repetido: </h3> $repetido "; 
 
 echo '<hr>';
  show_source(__FILE__); 
